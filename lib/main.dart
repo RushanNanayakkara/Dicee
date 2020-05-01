33f26899f1861b84cfac0ffc7d9c1bfde22864d0
rushannanayakkara <rushannana@gmail.com>
2020-05-01 18:24:37 +0530

    Complete application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,53 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNo = 6;
+  int rightDiceNo = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDiceNumbers();
+                  });
+                },
+                padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+                child: Image.asset("images/dice$leftDiceNo.png"),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDiceNumbers();
+                  });
+                },
+                padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+                child: Image.asset("images/dice$rightDiceNo.png"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void changeDiceNumbers() {
+    setState(() {
+      rightDiceNo = Random().nextInt(6) + 1;
+      leftDiceNo = Random().nextInt(6) + 1;
+    });
   }
 }
